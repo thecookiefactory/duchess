a17dde4e409e801e8271c53a4d0b2691dd756533
Bence Nagy <underer@gmail.com>
2014-02-18 08:02:10 +0100

    Add requirements.txt and use Redis instead of MySQL

--- duchess/__init__.py
+++ duchess/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_redis import Redis
 
 duchess = Flask(__name__)
 duchess.config.from_object('config')
-db = SQLAlchemy(duchess)
+redis_store = Redis(duchess)
 
 if duchess.debug:
     from flask.ext.debugtoolbar import DebugToolbarExtension
     toolbar = DebugToolbarExtension(duchess)
 
-from duchess.views import index
+import views
