b6ab70375726842e90446fb17fb4360861bb1413
Bence Nagy <underer@gmail.com>
2014-04-12 23:29:11 +0200

    Get port from env var

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+from os import getenv
+
 from duchess import create_app
 
 if __name__ == '__main__':
     duchess = create_app()
-    duchess.run(host='0.0.0.0')
+    duchess.run(host='0.0.0.0', port=int(getenv('PORT', 5000)))
