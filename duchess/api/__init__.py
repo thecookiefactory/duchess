e1a79bce313602b0bddf0ab7b11b40584bb53c54
Bence Nagy <underer@gmail.com>
2014-04-13 00:45:19 +0200

    Allow more methods on API calls

--- duchess/api/__init__.py
+++ duchess/api/__init__.py
@@ -5,6 +5,6 @@ api_router = Blueprint('api_router', __name__)
 duchess_api = API()
 
 
-@api_router.route('/api/<endpoint>')
+@api_router.route('/api/<endpoint>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def call_api(endpoint):
     return jsonify(getattr(duchess_api, endpoint)(request))
