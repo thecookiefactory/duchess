5f19dba99b3a640c2ed9c2c783a4566875b922d7
Bence Nagy <underer@gmail.com>
2014-04-13 02:43:28 +0200

    Make improvements to button styling

--- duchess/assets/js/index.js
+++ duchess/assets/js/index.js
@@ -1,5 +1,6 @@
 $(function(){
     $('section.hero .buttons .start').click(function(){
+        $('section.about').remove()
         $('section.hero').addClass('started');
         setTimeout(function(){$('section.hero video.intro').get(0).play();}, 2000);
     });
