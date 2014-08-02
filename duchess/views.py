fe87ea68c4d3bd1831a1b86fe7b9820d3bf637bf
Bence Nagy <bence@underyx.me>
2014-08-02 16:53:33 +0200

    Add broken admin panel

--- duchess/views.py
+++ duchess/views.py
@@ -6,3 +6,7 @@ def init_views(app):
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.route('/admin/')
+    def admin_panel():
+        return render_template('admin.html')
